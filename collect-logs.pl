58e2353e7de683eecac76b4ec192d283a78061da
Matt Paladino <575435+mpaladino@users.noreply.github.com>
2019-09-23 12:27:15 -0700

    refactoring perl version to retrieve logs from all live app and db servers

--- collect-logs.pl
+++ collect-logs.pl
@@ -15,12 +15,35 @@ sub help() {
 }
 
 if(defined $opt_u && defined $opt_e) {  # double check existence
-  print "Retrieving appserver logs...\r\n";
-  system('rsync -rlvz --size-only --ipv4 --progress -e \'ssh -p 2222\' ' . $opt_e . '.' . $opt_u . '@appserver.' . $opt_e . '.' . $opt_u . '.drush.in:logs/* app_server_logs');
-  print "Waiting for Appserver rsync to finish...\r\n";
-  sleep(15);
-  print "Retrieving DB server logs...\r\n";
-  system('rsync -rlvz --size-only --ipv4 --progress -e \'ssh -p 2222\' ' . $opt_e . '.' . $opt_u . '@dbserver.' . $opt_e . '.' . $opt_u . '.drush.in:logs/* db_server_logs');
+  if($opt_e eq 'live') {
+    # Run a dig to obtain all the IP address of the live application containers
+    print "Obtaining IP Address of appservers...\r\n";
+    @app_dig = `dig +short appserver.live.$opt_u.drush.in`;
+    # Begin retrieval of the log files on the live appservers
+    print "Retrieving live appserver " . $opt_u . " logs...\r\n";
+    foreach $app_server (@app_dig) {
+      chomp $app_server;
+      system('rsync -rlvz --size-only --ipv4 --progress -e \'ssh -p 2222\' ' . $opt_e . '.' . $opt_u . '@appserver.' . $opt_e . '.' . $opt_u . '.drush.in:logs/* app_server_' . $app_server .'_logs');
+      #print "Waiting for appserver rsync to finish...\r\n";
+      #sleep(3);
+    }
+    print "Obtaining IP Address of dbservers...\r\n";
+    @app_dig = `dig +short dbserver.live.$opt_u.drush.in`;
+    print "Retrieving DB server logs...\r\n";
+    foreach $app_server (@app_dig) {
+      chomp $app_server;
+      system('rsync -rlvz --size-only --ipv4 --progress -e \'ssh -p 2222\' ' . $opt_e . '.' . $opt_u . '@dbserver.' . $opt_e . '.' . $opt_u . '.drush.in:logs/* db_server_' . $app_server .'_logs');
+      #print "Waiting for dbserver rsync to finish...\r\n";
+      #sleep(3);
+    }
+  } else {
+    print "Retrieving appserver logs for " . $opt_e . "...\r\n";
+    system('rsync -rlvz --size-only --ipv4 --progress -e \'ssh -p 2222\' ' . $opt_e . '.' . $opt_u . '@appserver.' . $opt_e . '.' . $opt_u . '.drush.in:logs/* app_server_logs_' . $opt_e);
+    print "Waiting for Appserver rsync to finish...\r\n";
+    sleep(3);
+    print "Retrieving DB server logs for " . $opt_e . "...\r\n";
+    system('rsync -rlvz --size-only --ipv4 --progress -e \'ssh -p 2222\' ' . $opt_e . '.' . $opt_u . '@dbserver.' . $opt_e . '.' . $opt_u . '.drush.in:logs/* db_server_logs_' . $opt_e);
+  }
 } else {
   print "Some or all of the parameters are empty\r\n";
   help();
