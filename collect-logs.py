e32324e0b41f24000565515bb65a5fc24444ac25
Matt Paladino <575435+mpaladino@users.noreply.github.com>
2019-09-23 13:08:55 -0700

    adding in code to retrieve the multiple live appserver and dbserver log files

--- collect-logs.py
+++ collect-logs.py
@@ -17,17 +17,34 @@ args = parser.parse_args()
 
 # Check and see if UUID and ENV args were passed
 if ((not args.uuid) or (not args.env)):
-    parser.print_help()
-    # parser.print_usage() # for just the usage line
-    parser.exit()
+  parser.print_help()
+  # parser.print_usage() # for just the usage line
+  parser.exit()
+elif ( args.env == 'live'):
+  print("Beginning rsync of Live appserver logs...")
+  app_ipaddys = subprocess.Popen("dig +short appserver.live." + args.uuid + ".drush.in", shell=True, stdout=subprocess.PIPE)
+  app_ips = app_ipaddys.communicate()[0]
+  app_ips = app_ips.rstrip()
+  app_ips = app_ips.split('\n')
+  for ip in app_ips:
+    app_process = subprocess.Popen("rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' " + args.env + "." + args.uuid + "@appserver." + args.env + "." + args.uuid + ".drush.in:logs/* app_server_logs_" + ip, shell=True)
+    app_process.wait()
+  print("Beginning rsync of Live dbserver logs...")
+  app_ipaddys = subprocess.Popen("dig +short dbserver.live." + args.uuid + ".drush.in", shell=True, stdout=subprocess.PIPE)
+  app_ips = app_ipaddys.communicate()[0]
+  app_ips = app_ips.rstrip()
+  app_ips = app_ips.split('\n')
+  for ip in app_ips:
+    app_process = subprocess.Popen("rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' " + args.env + "." + args.uuid + "@dbserver." + args.env + "." + args.uuid + ".drush.in:logs/* db_server_logs_" + ip, shell=True)
+    app_process.wait()
 else:
   # Initiate rsync of application server logs
   # Open rsync subprocess and download the appserver logs
-  print("Beginning rsync of Appserver logs...")
+  print("Beginning rsync of " + args.env + " appserver logs...")
   app_process = subprocess.Popen("rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' " + args.env + "." + args.uuid + "@appserver." + args.env + "." + args.uuid + ".drush.in:logs/* app_server_logs", shell=True)
   app_process.wait()
 
   # Open rsync and retrieve the DB servers logs
-  print("Beginning rsync of DBserver logs...")
+  print("Beginning rsync of " + args.env + " DB server logs...")
   db_process = subprocess.Popen("rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' " + args.env + "." + args.uuid + "@dbserver." + args.env + "." + args.uuid + ".drush.in:logs/* db_server_logs", shell=True)
   db_process.wait()
