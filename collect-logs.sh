e5d2ec8a8586cd42f674e5f65052e2ef54395f30
Matt Paladino <575435+mpaladino@users.noreply.github.com>
2019-09-23 09:32:40 -0700

    adding in condition to detect live, then run dig to get list of app/db containers, and pull logs from each instance

--- collect-logs.sh
+++ collect-logs.sh
@@ -4,7 +4,7 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -uuid=YOURSITEUUID -env=YOURSITEENV"
+   echo "Usage: $0 -u=YOURSITEUUID -e=YOURSITEENV"
    echo -e "\t-u Site UUID from Dashboard URL"
    echo -e "\t-e Environment name such as dev, test or live"
    exit 1 # Exit script after printing help
@@ -42,10 +42,25 @@ fi
 # Grab newrelic.log, nginx-access.log, nginx-error.log, php-fpm-error.log, php-slow.log
 # and put them in an app_server directory where you execute this script
 # and put them in an app_server directory where you execute this script
-echo "Retrieving appserver logs...\n"
-rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV_NAME.$SITE_UUID@appserver.$ENV_NAME.$SITE_UUID.drush.in:logs/* app_server_logs
 
-# Grab the mysqld-slow-query.log, mysqld.log and put them in a 
-# db_server directory
-echo "Retrieving database server logs...\n"
-rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV_NAME.$SITE_UUID@dbserver.$ENV_NAME.$SITE_UUID.drush.in:logs db_server_logs
+# Check to see if the live environment is called, then grab the logs from all app containers
+echo "$ENV_NAME"
+if [ $ENV_NAME == 'live' ]; then
+  for app_server in `dig +short appserver.live.$SITE_UUID.drush.in`
+    do
+      echo "Retrieving appserver logs from $app_server..."
+      rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV_NAME.$SITE_UUID@appserver.$ENV_NAME.$SITE_UUID.drush.in:logs/* app_server_logs_$app_server
+    done
+  for app_server in `dig +short dbserver.live.$SITE_UUID.drush.in`
+    do
+      echo "Retrieving dbserver logs from $app_server..."
+      rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV_NAME.$SITE_UUID@dbserver.$ENV_NAME.$SITE_UUID.drush.in:logs db_server_logs_$app_server
+    done
+else
+   echo "Retrieving appserver logs..."
+   rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV_NAME.$SITE_UUID@appserver.$ENV_NAME.$SITE_UUID.drush.in:logs/* app_server_logs_$ENV_NAME
+   # Grab the mysqld-slow-query.log, mysqld.log and put them in a
+   # db_server directory
+   echo "Retrieving database server logs..."
+   rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV_NAME.$SITE_UUID@dbserver.$ENV_NAME.$SITE_UUID.drush.in:logs db_server_logs_$ENV_NAME
+fi
